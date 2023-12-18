a2ec6f48c401cb721d0e99058fb2f7eb5b1b3ca4
Sean Lucas <smlucas1993@gmail.com>
2023-12-18 12:55:49 -0500

    Learning more about LINQ

---
+++ Practice/Lessons/10_LINQCompositionExecution.cs
@@ -0,0 +1,56 @@
+﻿public class LINQCompositionExecution
+{
+    public static void Test()
+    {
+        var wordsA = new string[] { "cherry", "apple", "blueberry" };
+        var wordsB = new string[] { "cherry", "apple", "blueberry" };
+        bool match1 = wordsA.SequenceEqual(wordsB);
+        Console.WriteLine($"The sequences match: {match1}");
+
+        var wordsC = new string[] { "blueberry", "apple", "cherry" };
+        var wordsD = new string[] { "cherry", "apple", "blueberry" };
+        bool match2 = wordsA.SequenceEqual(wordsB);
+        Console.WriteLine($"The sequences match: {match2}");
+
+        int[] vectorA = { 0, 2, 4, 5, 6 };
+        int[] vectorB = { 1, 3, 5, 7, 8 };
+        var zipped = vectorA.Zip(vectorB, (a, b) => a * b);
+        Console.WriteLine("Multiplying values from 2 arrays.");
+        foreach (var num in zipped)
+        {
+            Console.WriteLine(num);
+        }
+
+        int[] numsA = { 0, 2, 4, 5, 6, 8, 9 };
+        int[] numsB = { 1, 3, 5, 7, 8 };
+        var pairs = from a in numsA
+                    from b in numsB
+                    where a < b
+                    select (a, b);
+        Console.WriteLine("Pairs where a < b");
+        foreach (var pair in pairs)
+        {
+            Console.WriteLine($"{pair.a} is less than {pair.b}");
+        }
+
+        int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+        var lowNumber = from n in numbers
+                        where n <= 3
+                        select n;
+        Console.WriteLine("First run numbers >= 3:");
+        foreach (int n in lowNumber)
+        {
+            Console.WriteLine(n);
+        }
+        //changing the values
+        for (int i = 0; i< 10; i++)
+        {
+            numbers[i] = -numbers[i];
+        }
+        Console.WriteLine("Second run numbers >= 3:");
+        foreach (int n in lowNumber)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
--- Practice/Lessons/9_LINQIntroduction.cs
+++ Practice/Lessons/9_LINQIntroduction.cs
@@ -1,9 +1,15 @@
-﻿public class LINQIntroduction
+﻿using System;
+
+public class LINQIntroduction
 {
     public static void Test()
     {
         List<int> digits = new List<int>() { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-        var lowNums = from num in digits where num < 5 select num;
+        var lowNums = from num
+                      in digits
+                      where num < 5
+                      select num;
+        Console.WriteLine("Getting all digits less than 5.");
         foreach (var num in lowNums)
         {
             Console.WriteLine(num);
@@ -11,17 +17,33 @@
 
         string[] strings = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
         var shortDigits = strings.Where((digits, index) => digits.Length < index);
+        Console.WriteLine("Getting strings shorter than their digit length.");
         foreach (var digit in shortDigits)
         {
-            Console.WriteLine($"The word {digit} is shorter th an its value.");
+            Console.WriteLine($"The word {digit} is shorter the an its value.");
         }
 
         var textNums = from n in digits
                        select strings[n];
+        Console.WriteLine("Matching number to string position.");
         foreach (var num in textNums)
         {
             Console.WriteLine(num);
         }
+
+        var first3Numbers = digits.Take(3);
+        Console.WriteLine("Getting first 3 digits.");
+        foreach (var num in first3Numbers)
+        {
+            Console.WriteLine(num);
+        }
+
+        var lessThan9Numbers = digits.TakeWhile(i => i < 9);
+        Console.WriteLine("Getting digits until one is found to be 9 or greater.");
+        foreach (var num in lessThan9Numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
-        
+
 }
