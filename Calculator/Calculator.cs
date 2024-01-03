6be9e9103c0558bbe0d57a0eda2d675968a83a41
Sean Lucas <smlucas1993@gmail.com>
2024-01-03 13:03:24 -0500

    added logic for C and CE, added TODOs tested and bug fixed a bit

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,15 +1,30 @@
-﻿using System.ComponentModel;
+﻿using System.IO;
 using System.Text;
 
 class Calculator
 {
+    private string displayValue = "";
+    //TODO add file path to log file, use same location as Calculator.cs
+    private string logFilePath = "";
+    //TODO add user inputs, calculations, etc to log as calculator runs!
+    private StringBuilder log = new StringBuilder();
 
+    //TODO fix performing a new calculation directly after pressing EQ not working correctly
+    //TODO add logging
+    //TODO move bulk of logic outside of run method and break apart to make more readable. run should be a lower level method
+    //TODO display ERR if any calculation has over 8 digits (trim down to 3 decimal places)
+    //TODO add 8 digit input restriction (not including decimal marker)
+    //TODO add 3 decimal place restriction on input
+    //TODO add +/- button to toggle positive or negative number
+    //TODO convert console output to actual buttons and display (create button classes!)
+    //TODO make sure logic is condense and readable
     public void run()
     {
-        Operator currentOp;
-        Operator? previousOp = null;
+        Boolean opPreviouslyUsed = false;
         StringBuilder currentVal = new StringBuilder();
         StringBuilder previousVal = new StringBuilder();
+        Operator currentOp;
+        Operator? previousOp = null;
 
         while (true)
         {
@@ -20,24 +35,42 @@ class Calculator
             {
                 currentOp = (Operator)Enum.Parse(typeof(Operator), userInput);
 
-                if (previousOp != Operator.EQ)
+                if (currentOp == Operator.C || currentOp == Operator.CE)
                 {
-                    if (previousVal.Length != 0)
-                        currentVal = Calculate(previousVal, currentVal, previousOp);
-
-                    previousVal.Clear();
-                    previousVal.Append(currentVal.ToString());
+                    if (currentOp == Operator.CE)
+                    {
+                        previousVal.Clear();
+                        previousOp = null;
+                    }
                     currentVal.Clear();
+                    this.displayValue = "";
+                    opPreviouslyUsed = false;
                 }
+                else
+                {
+                    if (previousOp != Operator.EQ && !opPreviouslyUsed)
+                    {
+                        if (previousVal.Length != 0)
+                            currentVal = Calculate(previousVal, currentVal, previousOp);
 
-                previousOp = currentOp;
+                        previousVal.Clear();
+                        previousVal.Append(currentVal.ToString());
+                        currentVal.Clear();
+                    }
+
+                    previousOp = currentOp;
+                    opPreviouslyUsed = true;
+                }
             }
             else
             {
                 currentVal.Append(userInput);
-                // TODO move this to logging
-                Console.WriteLine($"--- [DISPLAY INPUT VALUE] {currentVal.ToString()}");
+                opPreviouslyUsed = false;
+                this.displayValue = currentVal.ToString();
             }
+
+            Display();
+            Log();
         }
     }
 
@@ -46,6 +79,7 @@ class Calculator
         decimal calculatedValue;
         decimal pValue = Decimal.Parse(previousValue.ToString());
         decimal cValue = Decimal.Parse(currentValue.ToString());
+
         switch (op)
         {
             case Operator.ADD:
@@ -61,11 +95,21 @@ class Calculator
                 calculatedValue = pValue / cValue;
                 break;
             default:
-                throw new NotImplementedException("Missing operator! This should never be reached.");
+                throw new NotImplementedException($"Unsupported operator for calculations: {op.ToString()}");
         }
-        // TODO move this to logging
-        Console.WriteLine($"--- [DISPLAY CALCULATED VALUE] {calculatedValue.ToString()}");
+
+        this.displayValue = calculatedValue.ToString();
         return new StringBuilder(calculatedValue.ToString());
+    }
+
+    private void Display()
+    {
+        Console.WriteLine($"                [{this.displayValue}]");
+    }
+
+    private void Log()
+    {
+        File.WriteAllText(this.logFilePath + "log.txt", this.log.ToString());
     }
 
     private enum Operator
@@ -74,6 +118,8 @@ class Calculator
         SUB, 
         MUL, 
         DIV,
-        EQ
+        EQ,
+        C,
+        CE
     }
 }
